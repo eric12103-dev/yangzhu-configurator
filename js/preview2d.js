a12e90240a88fe9f7eda636719e286a1dc67496a
楊竹科技 <eric12103@gmail.com>
2026-05-19 20:23:57 +0800

    set rotation cursor on Fabric.js mtr control

--- js/preview2d.js
+++ js/preview2d.js
@@ -64,6 +64,12 @@ function init2DCanvas(productId) {
   fabric.Object.prototype.borderColor         = '#16a34a';
   fabric.Object.prototype.borderScaleFactor   = 2;
 
+  // 旋轉控制點改為旋轉游標
+  if (fabric.Object.prototype.controls?.mtr) {
+    const rotateCursorSvg = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='%23333' d='M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46A7.93 7.93 0 0 0 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74A7.93 7.93 0 0 0 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z'/%3E%3C/svg%3E") 12 12, grab`;
+    fabric.Object.prototype.controls.mtr.cursorStyle = rotateCursorSvg;
+  }
+
   canvas2d.on('selection:created', _showScaleBar);
   canvas2d.on('selection:updated', _showScaleBar);
   canvas2d.on('selection:cleared',  _hideScaleBar);
